e60f8e1007cd9f02aa903b55ea4e6fd36321bc84
joemamdouh12345 <48876276+joemamdouh12345@users.noreply.github.com>
2019-04-13 10:14:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1582,7 +1582,8 @@ google({ query: input, disableConsole: true }).then(results => {
 });
 
 }});
- function getValue(key, array) {
+ 
+function getValue(key, array) {
   for (var el in array) {
     if (array[el].hasOwnProperty(key)) {
       return array[el][key];
@@ -1598,18 +1599,18 @@ var prefix = "!";
     let args = message.content.split(' ').slice(1).join(' ');
 if(message.content.split(' ')[0] == prefix + 'bc') {
     if (!args[1]) {
-message.channel.send("!bc <message>");
+message.channel.send("**!bc <message>**");
 return;
 }
         message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
             var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', ${message.guild.name})
-            .addField('» المرسل : ', ${message.author.username}#${message.author.discriminator})
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
             .addField(' » الرسالة : ', args)
             .setColor('#ff0000')
-            // m.send([${m}]);
-            m.send(${m},{embed: bc});
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
         });
     }
     } else {
